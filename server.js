50ac10fc8ef4860b9de1060a6add8c40f824a1a5
Ethan Parizeau <ethanparizeau@gmail.com>
2018-12-20 18:47:16 -0600

    forgot server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 const PORT = 8000;
 
@@ -11,6 +12,14 @@ app.use(express.static(DIST_DIR));
 
 app.get('/', (req, res) => {
     res.sendFile(HTML_FILE);
+});
+
+// Bug Report
+app.post('/bugReport/:bug', (req, res) => {
+    fs.appendFile('bugLog.txt', new Date().toLocaleString() + "- " + req.params.bug + "\r\n", (err) => {
+        if(err) console.log(err);
+    })
+    console.log(new Date().toLocaleString() + ": " + req.params.bug);
 });
 
 app.listen(PORT, () => {
